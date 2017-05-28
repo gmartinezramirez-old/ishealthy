5a7865366985d07cd3098f8985f7beb83a39394c
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2017-05-28 17:50:17 -0400

    ADD: class with setters and getters to send image to Cognitive services

---
+++ app/main/java/com/gmartinezramirez/healthyfood/ai/CustomVisionURLImage.java
@@ -0,0 +1,19 @@
+package com.gmartinezramirez.healthyfood.ai;
+
+public class CustomVisionURLImage {
+    private String Url;
+
+    public CustomVisionURLImage() {}
+
+    public CustomVisionURLImage(String url) {
+        Url = url;
+    }
+
+    public String getUrl() {
+        return Url;
+    }
+
+    public void setUrl(String url) {
+        Url = url;
+    }
+}
